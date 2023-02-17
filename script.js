3de6180e6b94ace428be5a1384aebdfd57fd889b
Magellan994 <khukha994@gmail.com>
2023-02-17 13:46:44 +0300

    first

--- script.js
+++ script.js
@@ -1,25 +1,34 @@
-/////// дз 7
+/////// дз 6
 
+let userDate = prompt('year of birth')
+let userCity = prompt('Which city')
+let kindeOfSport = prompt('Which sport')
 
-let numOrStr = prompt('input number or string');
-
-switch (true) {
-
-    case numOrStr === null:
-        console.log('ви скасували')
-
-        break;
-
-    case numOrStr.trim() === '':
-        console.log('Empty String')
-        break;
-
-    case isNaN( +numOrStr ):
-        console.log('number is Ba_NaN')
-        break;
-
-    default:
-        console.log('OK!')
+if (userDate !== null){
+    alert(`your age is ${2023 - userDate}`)
+    if (userCity === 'Kiev'){
+        alert('capital of Ukraine');
+    }else if (userCity ==='London'){
+        alert('capital of UK');
+    }else if (userCity ==='Washington'){
+        alert('capital of USA');
+    }else {
+        alert(`your city is ${userCity}`)
+    }
+    if (kindeOfSport === 'box'){
+        alert('do you want t be like a M.Ali?')
+    }else if(kindeOfSport === 'football'){
+            alert('do you want to be like a L.Messi ?')
+    }else if(kindeOfSport === 'basketball'){
+        alert('do you want to be like a M.Jordan ?')
+    }else {
+        alert('also good!')
+    }
+}else{
+    alert(`so sorry that you do not like 
+    to inform us about your date of birth, where do you live
+    ,and kinde of sport that do you like .... `)
 }
+
 
 
