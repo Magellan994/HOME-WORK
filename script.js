6ffc1008f4b6fdc8e722983515301150cebf19dd
Magellan994 <khukha994@gmail.com>
2023-02-19 17:32:35 +0300

    hm6

--- script.js
+++ script.js
@@ -1,32 +1,34 @@
+let userDate = prompt('year of birth')
+let userCity = prompt('Which city')
+let kindeOfSport = prompt('Which sport');
+
+let userAge = 'your age'
+let userCityInfo = 'your city';
+let userSport ='kind of sport'
+
+if (userDate === null) {
+    alert(`you dont like to inform us about ${userAge} ,${userCityInfo},${userSport}`);
 
 
-let a;
-
-let b;
-
-let c;
-
-a = 34
-
-b = 20
-
-c = 'hello'
-a = a + 2;
-
-b = b - 5;
-
-c = c + " world!";
-
-console.log(a); // => 36
-console.log(b);// => 15
-
-console.log(c); // => "Hello world!"
-
-const userNumber = prompt("?")
-
-let k = userNumber * 2;
-
-k = k + 7
-
-
-////////////////////
+}else {
+    userAge = `your age is ${2023 - userDate}`
+    if (userCity === 'Kiev'){
+        userCityInfo='your city is a capital of Ukraine';
+    }else if (userCity ==='London'){
+        userCityInfo ='your city is a capital of UK';
+    }else if (userCity ==='Washington'){
+        userCityInfo ='capital of USA';
+    }else {
+        userCityInfo = `your city is ${userCity}`
+    }
+    if (kindeOfSport === 'Box' || 'box'){
+        userSport ='do you want t be like a M.Ali?'
+    }else if(kindeOfSport === 'Football' || 'football'){
+        userSport='do you want to be like a L.Messi ?'
+    }else if(kindeOfSport  === 'Basketball' || 'basketball'){
+        userSport='do you want to be like a M.Jordan ?'
+    }else {
+        userSport = 'also good!'
+    }
+    alert(`${userAge},${userCityInfo},${userSport}`)
+}
