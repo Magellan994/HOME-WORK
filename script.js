7ce7ccfd1454f85f00a738f2f5c73e9b1da06439
Magellan994 <khukha994@gmail.com>
2023-02-24 00:59:52 +0300

    hm7.4....

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 /////// дз 7.4
+/////// дз 8
 
-let x = 20;
+let x = 13;
+let a = true;
 
-d:
-    for (let  i = 2; i <= x; i++){
-        for (let j = 2; j < i; j++){
-            if (i % j === 0) continue d
+
+if (x === 1){
+    console.log('1 is neither prime')
+}else if (x > 1) {
+    for (let i = 2; i < x; i++) {
+        if (x % i === 0) {
+            a = false;
         }
-        console.log(i)
     }
+
+    if (a) {
+        console.log(`${x} is prime`)
+    } else {
+        console.log(`${x} is not prime`)
+    }
+}else {
+    console.log('this number is not a prime number')
+}
