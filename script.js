bfecf58868512082884d21f4dbd113c9913c6186
Magellan994 <khukha994@gmail.com>
2023-02-27 13:43:13 +0300

    h-w9

--- script.js
+++ script.js
@@ -1,24 +1,92 @@
-/////// дз 7.4
-/////// дз 8
-
-let x = 13;
-let a = true;
+let arr =[];
+let arr2 =[]
+let arr3 =[]
 
 
-if (x === 1){
-    console.log('1 is neither prime')
-}else if (x > 1) {
-    for (let i = 2; i < x; i++) {
-        if (x % i === 0) {
-            a = false;
-        }
+
+let users = [
+    {
+        "index": 0,
+        "isActive": true,
+        "balance": "$2,226.60",
+        "name": "Eugenia Sawyer",
+        "gender": "female",
+        "phone": "+1 (840) 583-3207",
+        "address": "949 John Street, Rose, Puerto Rico, 1857"
+    },
+    {
+        "index": 1,
+        "isActive": true,
+        "balance": "$2,613.77",
+        "name": "Pauline Gallegos",
+        "gender": "female",
+        "phone": "+1 (985) 593-3328",
+        "address": "328 Greenpoint Avenue, Torboy, North Dakota, 6857"
+    },
+    {
+        "index": 2,
+        "isActive": false,
+        "balance": "$3,976.41",
+        "name": "Middleton Chaney",
+        "gender": "male",
+        "phone": "+1 (995) 591-2478",
+        "address": "807 Fleet Walk, Brutus, Arkansas, 9783"
+    },
+    {
+        "index": 3,
+        "isActive": true,
+        "balance": "$1,934.58",
+        "name": "Burns Poole",
+        "gender": "male",
+        "phone": "+1 (885) 559-3422",
+        "address": "730 Seba Avenue, Osage, Alabama, 6290"
+    },
+    {
+        "index": 4,
+        "isActive": true,
+        "balance": "$3,261.65",
+        "name": "Mcfadden Horne",
+        "gender": "male",
+        "phone": "+1 (942) 565-3988",
+        "address": "120 Scholes Street, Kirk, Michigan, 1018"
+    },
+    {
+        "index": 5,
+        "isActive": false,
+        "balance": "$1,790.56",
+        "name": "Suzette Lewis",
+        "gender": "female",
+        "phone": "+1 (837) 586-3283",
+        "address": "314 Dunne Place, Bawcomville, Guam, 9053"
     }
+]
 
-    if (a) {
-        console.log(`${x} is prime`)
-    } else {
-        console.log(`${x} is not prime`)
+
+
+const filtredArray = users.filter((item) => {
+    if ( item.balance >'$2,000') {
+        return arr.push(item.phone);
     }
-}else {
-    console.log('this number is not a prime number')
-}
+});
+
+
+const filtredArray2 = users.filter((item) => {
+    if ( item.balance > '$2,000') {
+        return arr2.push (item.balance.slice(1,6).split(/,\s*/).join(''))
+
+    }
+});
+
+
+arr2.map(arr4 => {
+    arr3.push(parseInt(arr4))
+
+})
+
+const sum  = arr3.reduce((a,b) => a + b )
+
+
+console.log(filtredArray)
+console.log(arr)
+console.log(filtredArray2)
+console.log( sum)
